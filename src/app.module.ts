578c0a4f70fbb478bf21c1c0101e4fb9daaa560d
taeheon <dalo952003@gmail.com>
2021-12-10 00:50:06 +0900

    change db mongo to postgresql and change orm mongoose to TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,28 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ListModule } from './list/list.module';
 import { CardModule } from './card/card.module';
-import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { TodoModule } from './todo/todo.module';
+import { BoardModule } from './board/board.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ListModule,
     CardModule,
     ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.MONGO_URI),
+    TodoModule,
+    BoardModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
